676d42a616ec28e984bdc838021c9e7c70ce4a85
Rafael-Rodrigues18 <Rafinhasegui@gmail.com>
2026-03-29 13:13:27 -0300

    Criando meu projeto de Pw

---
+++ AplicacaoBanco/Program.cs
@@ -0,0 +1,29 @@
+using AplicacaoBanco.Repository;
+using AplicacaoBanco.Repository.Contract;
+
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
